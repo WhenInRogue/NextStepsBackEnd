3e82a773053959df761e9c93cbae8eacddee5918
WhenInRogue <Zajomapijoseph@gmail.com>
2026-08-20 22:42:31 -0500

    second commit

---
+++ src/test/java/com/WhenInRogue/NextSteps/NextStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WhenInRogue.NextSteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
